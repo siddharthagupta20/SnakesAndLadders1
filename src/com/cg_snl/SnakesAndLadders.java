1ae41ebb7e92b73463b912fc07031b118230d863
siddharthagupta20 <siddharthagupta20@gmail.com>
2020-09-26 10:15:27 +0530

    modified: added other player and ladder condition

--- src/com/cg_snl/SnakesAndLadders.java
+++ src/com/cg_snl/SnakesAndLadders.java
@@ -5,44 +5,92 @@ import java.util.Random;
 public class SnakesAndLadders {
 
 	public static void main(String[] args) {
-		int pos = 0;
 
+		int pos1 = 0, pos2 = 0;
 		Random rand = new Random();
-		int turn = 0;
-		while (pos != 100) {
-			int dice = rand.nextInt(6) + 1;
+		boolean player1Playing = true;
+		int noOfTurns = 1;
 
-			int option = rand.nextInt(3);
-			switch (option) {
-			case 0:
+		while (pos1 != 100 && pos2 != 100) {
 
-				break;
-			case 1:
+			boolean onLadder = true;
 
-				pos = pos + dice;
-				break;
-			case 2:
+			if (player1Playing) {
 
-				pos = pos - dice;
-				break;
+				int lastRoll = 0;
+
+				while (onLadder) {
+					int dice = rand.nextInt(6) + 1;
+					int option = rand.nextInt(3);
+
+					switch (option) {
+
+					case 0:
+
+						onLadder = false;
+						break;
+
+					case 1:
+
+						lastRoll = dice;
+						pos1 += lastRoll;
+						break;
+
+					case 2:
+
+						pos1 = pos1 - dice;
+						onLadder = false;
+						break;
+					}
+				}
+				if (pos1 > 100)
+					pos1 -= lastRoll;
+				if (pos1 < 0)
+					pos1 = 0;
+				if (pos1 == 100) {
+					System.out.println("Player 1 Won.");
+					System.out.println("Total No. of turns: " + noOfTurns);
+					System.out.println("Position of Player1: " + pos1 + "\nPosition of Player2: " + pos2);
+					break;
+				}
+				player1Playing = false;
+
+			} else {
+
+				int lastRoll = 0;
+				while (onLadder) {
+					int dice = rand.nextInt(6) + 1;
+					int option = rand.nextInt(3);
+					switch (option) {
+					case 0:
+						onLadder = false;
+						break;
+					case 1:
+						lastRoll = dice;
+						pos2 = pos2 + lastRoll;
+						break;
+					case 2:
+						onLadder = false;
+						pos2 = pos2 - dice;
+						break;
+					}
+				}
+				if (pos2 > 100)
+					pos2 -= lastRoll;
+				if (pos2 < 0)
+					pos2 = 0;
+				if (pos2 == 100) {
+					System.out.println("Player 2 Won.");
+					System.out.println("Total No. of turns: " + noOfTurns);
+					System.out.println("Position of Player1: " + pos1 + "\nPosition of Player2: " + pos2);
+					break;
+				}
+				player1Playing = true;
 
 			}
-			if (pos > 100) {
-				pos -= dice;
-
-			}
-
-			if (pos < 0)
-				pos = 0;
-
-			System.out.println("New Position: " + pos);
-			if (pos == 100) {
-				break;
-			}
-
-			turn++;
+			noOfTurns++;
 		}
-		System.out.println("Turns: " + turn);
+
 	}
 
 }
